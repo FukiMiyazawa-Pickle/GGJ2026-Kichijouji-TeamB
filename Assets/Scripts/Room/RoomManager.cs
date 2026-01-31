3d975c9bbb95ab94d1d9ed496fe4bf40f6caaa40
MSI\JohnKang <s901052002@yahoo.com.tw>
2026-01-31 17:42:58 +0900

    部屋シーン追加

---
+++ Assets/Scripts/Room/RoomManager.cs
@@ -0,0 +1,37 @@
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class RoomManager : MonoBehaviour
+{
+    public List<Sprite> wallSprites;
+    public List<RoomObject> wallObjects;
+    public List<Sprite> floorSprites;
+    public List<RoomObject> floorObjects;
+
+    public SpriteRenderer wall;
+    public SpriteRenderer floor;
+
+    private void Awake() {
+        if (wallSprites.Count > 0) {
+            wall.sprite = wallSprites[Random.Range(0, wallSprites.Count)];
+            wall.transform.position = new Vector2(Random.Range(-18f, 18f), Random.Range(6f, 8f));
+        }
+
+        if (wallObjects.Count > 0)
+            Instantiate(wallObjects[Random.Range(0, wallObjects.Count)], new Vector2(Random.Range(-16f, 16f), Random.Range(1.6f, 2.6f)), Quaternion.identity, transform);
+
+        if (floorSprites.Count > 0) {
+            floor.sprite = floorSprites[Random.Range(0, floorSprites.Count)];
+            floor.transform.position = new Vector2(Random.Range(-11f, 11f), Random.Range(-0.5f, -6f));
+        }
+
+        if (floorObjects.Count > 0) {
+            Vector2 positino;
+            for (int i = 0; i < 5; ++i) {
+                positino = new Vector2(Random.Range(-17f, 17f), Random.Range(2f, -10f));
+                Instantiate(floorObjects[Random.Range(0, floorObjects.Count)], positino, Quaternion.identity, transform);
+            }
+        }
+    }
+}
